2fc86bda806314b906a24123e70bf41c8276713a
Abinash1501 <139610263+Abinash1501@users.noreply.github.com>
2025-05-17 21:57:51 +0530

    Update Gold_prediction_App.py

--- Gold_prediction_App.py
+++ Gold_prediction_App.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from tensorflow.keras.models import load_model
-#import joblib
+from keras.models import load_model
+import joblib
 from datetime import datetime, timedelta
 
 
